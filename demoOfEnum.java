15ee1712c81b175d67d89cd860c21def25185eb0
shalini058 <132085825+shalini058@users.noreply.github.com>
2025-01-26 19:09:30 +0530

    demoOfEnum.java

--- demoOfEnum.java
+++ demoOfEnum.java
@@ -7,14 +7,26 @@ public class demoOfEnum
 	public static void main(String[] args) {
 	    Example eg= Example.CAR;// using dot we can access enum constants.
 	    System.out.println(eg.ordinal());// using ordinal we find order of enum constants. 
-	    System.out.println(eg);
+	    System.out.println(eg); //eg holds an object of type Example
 	    
 	    //using for loop we return the enum array.
 	    for(Example ss:Example.values())//enum type has a values() method, which returns an array of all enum constants
 	    {
 	        System.out.println(ss +" : "+ ss.ordinal());// here it's return constants and their order.
 	    }
-	  
+ // In enum, we used switch statement to check for corresponding values.
+		switch(eg) //eg 
+		{ 
+			case CAR:
+				System.out.println("It's running"); //If 'eg' is equal to 'Example.CAR'.
+				break;
+			case SPEED:
+				System.out.println("It's in normal speed");
+				break;
+			case TIME: // along this we can use default because its last.
+				System.out.println("It's 7:03 pm");
+				break;
+		}
 	}
 }
 
