9f94c39fc14379b1905cb5441669e3dbc715f2fb
shalini058 <132085825+shalini058@users.noreply.github.com>
2025-01-28 22:41:46 +0530

    exampleOfExceptions.java
    Before exception we have to know about Errors.
    
    there are different type of errors.
    Compile time error 
    Runtime error
    Logical error
    
    Compile time error -->Syntactical or syntax error
    
    Logical Error --> bugs --> Logic are wrong are called Logical Error
    
    Runtime Error --> When the program suddenly stop in between. Basically exception is runtime error.
    
    There are two types of statement
    1. Normal statement 
    2. Critical statement --> Runtime Error Or throws some error
    
    Exception: When executing Java code, different errors can occur: coding errors made by the programmer, errors due to wrong input, or other unforeseeable things.
    
    We need to handle the exceptions.
    
    # If the statement is critical, then to handle the exception we need to try to execute the code.
    
    
     TRY:- It's trying to handle the errors in java.
    
    If java tries to execute the code and there will be an error then it will throw the error.
    
    
    Try will basically try to execute the statements , if everything goes good it will execute the remaining statement.
    If something goes wrong, if there is an exception it will execute the catch block.
    
    CATCH: It will catch the error.
    you will create an object or you will refer that with an exception class.
    
    If try block is not working then catch will be execute the code.
    
    Catch block will be executed only in case of exception, otherwise catch block will be skipped.

---
+++ exampleOfExceptions.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                Exception: When executing Java code, different errors can occur
+                coding errors made by the programmer, errors due to wrong input, 
+                       or other unforeseeable things.
+
+*******************************************************************************/
+//If the statement is critical, then to handle the exception we need to try to execute the code.
+
+public class exampleOfExceptions
+{
+	public static void main(String[] args) {
+		int a=9;
+		int b=3;
+		//It's trying to handle the errors in java.
+		try
+		{
+		    //if b=0 then it will throw an error.
+		    System.out.println(a/b); //If java tries to execute the code and there will be an error then it will throw the error.
+		}
+		
+		//It will catch the error. 
+		catch(Exception e)//here you will create an object or you will refer that with an exception class
+		{
+		    System.out.println("there is something wrong...");//If try block is not working then catch will be execute the code.
+		}
+		
+		System.out.println(b);
+	}
+}
+
